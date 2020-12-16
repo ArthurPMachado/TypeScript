fde59b443b7f81d933e1e180ec34d403137f99b6
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-16 14:14:55 -0300

    Feat: explore number and boolean types

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -1,3 +1,11 @@
 "use strict";
+// string
 var nome = 'João';
 console.log(nome);
+// numbers
+var idade = 27;
+idade = 27.5;
+console.log(idade);
+// boolean
+var possuiHobbies = false;
+console.log(possuiHobbies);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,2 +1,12 @@
+// string
 const nome = 'João';
 console.log(nome);
+
+// numbers
+let idade = 27;
+idade = 27.5;
+console.log(idade);
+
+// boolean
+const possuiHobbies = false;
+console.log(possuiHobbies);
