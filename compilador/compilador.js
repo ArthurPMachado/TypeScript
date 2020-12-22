61220684aeca10d7190d6e48e57dbf5d87f60a4e
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-22 12:37:31 -0300

    Feat: explore noImplicitAny

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -4,4 +4,10 @@ var inscritos = 610234;
 // canal = inscritos;
 console.log("Canal = " + canal);
 // let nome = 'Ricardo';
+function soma(a, b) {
+    return a + b;
+}
+var qualquerCoisa;
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
 //# sourceMappingURL=compilador.js.map
