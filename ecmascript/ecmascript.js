c716d6b9e58fb8418c96617f1c42f834e0f44408
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-24 11:38:24 -0300

    Feat: explore arrow function

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -8,6 +8,15 @@ if (estarFrio) {
     console.log(acao);
 }
 const cpf = '123.456.000-99';
-cpf = '789.101.132-78';
+// cpf = '789.101.132-78';
 console.log(cpf);
+// Arrow function 
+function somar(n1, n2) {
+    return n1 + n2;
+}
+console.log(somar(1, 1));
+const subtrair = (n1, n2) => n1 - n2;
+console.log(subtrair(1, 1));
+const saudacao = () => console.log("Olá");
+saudacao();
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -9,5 +9,18 @@ if(estarFrio) {
 }
 
 const cpf: string = '123.456.000-99';
-cpf = '789.101.132-78';
+// cpf = '789.101.132-78';
 console.log(cpf);
+
+// Arrow function 
+function somar(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+console.log(somar(1, 1));
+
+const subtrair = (n1: number, n2: number): number => n1 - n2;
+console.log(subtrair(1, 1));
+
+const saudacao = () => console.log("Olá");
+saudacao();
