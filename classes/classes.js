36ccce7387eaafce0a6bce4f749d5259a327396e
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-01-03 12:26:04 -0300

    Feat: creat object with method

--- classes/classes.js
+++ classes/classes.js
@@ -27,4 +27,21 @@ console.log(aniversarioEnxuto);
 const casamentoEnxuto = new DataEnxuta; // pode omitir os "()"
 casamentoEnxuto.ano = 2017;
 console.log(casamentoEnxuto);
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+    aplicarDesconto() {
+        if (this.desconto > 0) {
+            this.preco = this.preco - (this.preco * this.desconto);
+        }
+    }
+}
+const produtoSemDesconto = new Produto("Sofá", 999.99);
+console.log(produtoSemDesconto);
+const produtoComDesconto = new Produto("Televisão", 1599.99, 0.15);
+produtoSemDesconto.aplicarDesconto();
+console.log(produtoComDesconto);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -34,3 +34,22 @@ console.log(aniversarioEnxuto);
 const casamentoEnxuto = new DataEnxuta; // pode omitir os "()"
 casamentoEnxuto.ano = 2017;
 console.log(casamentoEnxuto);
+
+class Produto {
+  constructor(public nome: string, public preco: number, 
+    public desconto: number = 0) {
+    
+  }
+
+  aplicarDesconto() {
+    if(this.desconto > 0) {
+      this.preco = this.preco - (this.preco * this.desconto);
+    }
+  }
+}
+
+const produtoSemDesconto = new Produto("Sofá", 999.99);
+console.log(produtoSemDesconto);
+const produtoComDesconto = new Produto("Televisão", 1599.99, 0.15);
+produtoSemDesconto.aplicarDesconto();
+console.log(produtoComDesconto);
