d344326620a17ffbd9cfa11370e9607b4c2639fc
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-01-06 11:34:53 -0300

    Feat: use <reference path />

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,24 +1,7 @@
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        var PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-var PI = 2.99;
+"use strict";
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRet.ts"/>
+const PI = 2.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
+//# sourceMappingURL=namespaces.js.map
